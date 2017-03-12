92c95783eced8918a557b5e69f028674badf7350
JCornford <jonathan.cornford@gmail.com>
2017-03-12 15:52:36 +0000

    simulation working v1

--- js/visualisation.js
+++ js/visualisation.js
@@ -15,7 +15,7 @@ slider4.onchange = update;
 // set the dimensions and margins of the graph
 var margin = {top: 20, right: 20, bottom: 30, left: 50}
 var width  = 600; // this is pixels
-var height = 400;
+var height = 300;
 
 // set the ranges and scale to the range of the data!
 // the scale is normmally split off..
@@ -52,12 +52,13 @@ function init() {
 	
 	const values = calculate(a, b, c, d);
 
-	console.log(values);
+	//console.log(values);
 	// Scale the range of the data - we want a pad here 
 	const pad_y = 25;
   	//x_scale.domain(d3.extent(values, function(d) {return d; }));
   	x_scale.domain([0, values.length*dt]);
-  	y_scale.domain([0-pad_y, d3.max(values, function(d) { return d; })+pad_y]);
+  	//y_scale.domain([0-pad_y, d3.max(values, function(d) { return d; })+pad_y]);
+  	y_scale.domain([-80, 40]);
 
 
 	// console.log(values);
@@ -86,7 +87,10 @@ function update() {
 }
 
 function calculate(a, b, c, d) {
-
+	a = 0.1
+	b = 0.2
+	c = -65.0
+	d = 2.0
 	const klow    = 1.7   // nS/mV
 	const khigh   = 14    // nS/mV
 	const vthresh = -45   // mV
@@ -95,8 +99,7 @@ function calculate(a, b, c, d) {
 	const Cm      = 90    // membrane capacitance
 	const ishift  = 0
 	
-	var u = v0*b; // v0 and tstop are still in global scope - bit shit
-	var v = v0;
+	
 
 	// make time array
 	const t = [];
@@ -110,27 +113,41 @@ function calculate(a, b, c, d) {
 	const i_stim = [];
 	for (let i = 0; i < t.length; i++ ){
 		if (t[i] > 200 && t[i]<800) {
-			i_stim.push(300) // this should be slider as well
+			i_stim.push(5) // this should be slider as well
 		} else {
-			i_stim.push(0)
+			i_stim.push(0);
 		}
 	}
 	//console.log(i_stim.length);
 
 	// calculate voltage values
 	const v_values = [];
-	for (let i = 1; i < tstop + 1; i++) {
-		//v_values.push(Math.random() * -10);
-		v = 20
-		if (v < vthresh ){
-			var k = klow 
-		} else {
-			k = khigh
-		}
+	const u_values = [];
+	var u = v0*b; // v0 and tstop are still in global scope - bit shit
+	var v = v0;
+	for (let i = 1; i < t.length; i++) {
+		// not actually using the k thing as diff implementation
+		//if (v < vthresh ){
+		//	var k = klow;
+		//} else {
+		//	k = khigh;
+		//};
 		
+		v = v+( (0.04*Math.pow(v,2) + 5*v +140) - u + i_stim[i] )*dt ;
+		u = u+( a*(b*v-u) )*dt;
+
+		if (v >= 30){ // don't need v thresh in simple implementation either
+			v_values.push(30);
+			v = c;
+			u = u+d;
+		} else {
+			v_values.push(v);
+			u_values.push(u);
+		}
 	}
 	// in the end did not return all three as array as can't work it!
-	return i_stim;
+	//console.log(u_values);
+	return v_values;
 }
 
 init();
